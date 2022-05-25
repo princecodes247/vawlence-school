4590e2be8312874f9f9d4fd5ded4d9674650f5fe
Prince Codes <princecodes247@gmail.com>
2022-05-25 10:14:23 -0400

    create layout system

---
+++ components/Layout.js
@@ -0,0 +1,31 @@
+import React, { ReactNode } from 'react'
+// import { NextSeo } from 'next-seo'
+import { motion } from 'framer-motion'
+
+const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+}
+
+const Layout = ({ children, title, description }) => (
+    <div>
+        {/* <NextSeo title={title} description={description} openGraph={{ title, description }} /> */}
+        <motion.main
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ type: 'linear' }}
+            className="
+                    flex flex-col items-start w-full pt-10
+                    px-8 sm:px-16 md:px-36 lg:px-52 xl:px-80 2xl:px-96
+                    pt-24 h-full
+                "
+        >
+            {children}
+        </motion.main>
+    </div>
+)
+
+export default Layout
