d83c06dff7c73fadb835169870cc3cfa6fefcfb3
Prince Codes <princecodes247@gmail.com>
2022-05-25 10:13:36 -0400

    feat: create footer

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by PrinceCodes
+        </a>
+      </footer>
+)
+
+export default Footer
