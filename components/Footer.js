3b9aa208c15c703f3e2b9eed8209c782d1ae9d49
Prince Codes <princecodes247@gmail.com>
2022-05-25 11:27:07 -0400

    fix: undefined style reference

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 
 const Footer = () => (
-    <footer className={styles.footer}>
+    <footer className=''>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
