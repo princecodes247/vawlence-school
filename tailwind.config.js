1881152ab255da22672183cd4641a3d0a9173285
Prince Codes <princecodes247@gmail.com>
2022-06-01 07:19:32 -0400

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#495732"
+      },
+      fontFamily: {
+        'body': ['"Open Sans"', ],
+        'header': ['"Syne"', ],
+      }
+      
+    },
+
   },
   plugins: [],
 }
