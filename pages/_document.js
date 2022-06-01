83df99c028555cbd75492cdfbd30de850c75425f
Prince Codes <princecodes247@gmail.com>
2022-06-01 09:43:56 -0400

    add fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Syne:wght@400;600;700&display=swap" rel="stylesheet" /> 
         </Head>
         <body>
           <Main />
