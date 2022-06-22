fd880099560e71b328c4ec3499aff5fdd4ff2dad
Prince Codes <princecodes247@gmail.com>
2022-06-22 05:57:46 -0400

    Style Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,22 @@
-
 const Footer = () => (
-    <footer className='p-4 text-center bg-gray-100'>
-        <a
-          href="https://princecodes.vercel.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-semibold"
-        >
-          Created by PrinceCodes
-        </a>
-      </footer>
-)
+  <footer className="flex flex-col items-center gap-4 p-4 text-center mt-12 bg-gray-100">
+    <a
+      href="https://princecodes.vercel.app"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="font-semibold text-primary underline underline-primary"
+    >
+      Created by PrinceCodes
+    </a>
+    <a
+      href="https://www.github.com/princecodes247"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="font-semibold text-primary underline underline-primary"
+    >
+      Github
+    </a>
+  </footer>
+);
 
-export default Footer
+export default Footer;
