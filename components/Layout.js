75b9322ae467fcea017aa2823fad3cc7a20281ab
Prince Codes <princecodes247@gmail.com>
2022-06-22 16:04:41 -0700

    Add Adsense Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,30 +1,36 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
 // import { NextSeo } from 'next-seo'
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
+import Script from "next/script";
 
 const variants = {
-    hidden: { opacity: 0, x: 0, y: -5 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: 10 },
-}
+  hidden: { opacity: 0, x: 0, y: -5 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: 10 },
+};
 
 const Layout = ({ children, title, description }) => (
-    <div>
-        {/* <NextSeo title={title} description={description} openGraph={{ title, description }} /> */}
-        <motion.main
-            initial="hidden"
-            animate="enter"
-            exit="exit"
-            variants={variants}
-            key="appBody"
-            transition={{ type: 'linear', default: { duration: 0.5 }, }}
-            className="
+  <div>
+    {/* <NextSeo title={title} description={description} openGraph={{ title, description }} /> */}
+    <motion.main
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      key="appBody"
+      transition={{ type: "linear", default: { duration: 0.5 } }}
+      className="
                     flex flex-col items-start w-full min-h-screen
                 "
-        >
-            {children}
-        </motion.main>
-    </div>
-)
+    >
+      {children}
+    </motion.main>
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9274160991328583"
+      crossorigin="anonymous"
+    />
+  </div>
+);
 
-export default Layout
+export default Layout;
