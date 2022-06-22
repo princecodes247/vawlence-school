1a75f061c080251b1b2b4566c6fa4b62c8b6d1cc
Prince Codes <princecodes247@gmail.com>
2022-06-22 06:09:25 -0400

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <footer className="flex flex-col items-center gap-4 p-4 text-center mt-12 bg-gray-100">
     <a
-      href="https://princecodes.vercel.app"
+      href="https://www.github.com/princecodes247"
       target="_blank"
       rel="noopener noreferrer"
       className="font-semibold text-primary underline underline-primary"
@@ -9,12 +9,12 @@ const Footer = () => (
       Created by PrinceCodes
     </a>
     <a
-      href="https://www.github.com/princecodes247"
+      href="https://princecodes-2022.vercel.app"
       target="_blank"
       rel="noopener noreferrer"
       className="font-semibold text-primary underline underline-primary"
     >
-      Github
+      Portfolio (under Construction)
     </a>
   </footer>
 );
